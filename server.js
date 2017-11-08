8a7d9cc380c35afd6dbf433679c6961e7abde107
Rony Aparicio <u6009311@ad.utah.edu>
2017-11-08 11:33:04 -0700

    ready for front end

--- server.js
+++ server.js
@@ -13,16 +13,6 @@ app.use(express.static("client/build"));
 // Add routes, both API and view
 app.use(routes);
 
-// Set up promises with mongoose
-mongoose.Promise = global.Promise;
-// Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
-  {
-    useMongoClient: true
-  }
-);
-
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
