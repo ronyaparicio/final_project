628a867b5425a5017b6973dea30a26b51bfa4104
rony <ronyaparicio1@gmail.comgit config --global user.name ronygit commit -m 'l'git config --global user.email ronyaparicio1@gmail.com>
2017-11-07 19:30:42 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const path = require('path');
+const app = express();
+
+const routes = require("./controllers/controller.js");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
