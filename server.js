b4d6d45cf58a88fba3da2640f303048ade72b5c0
Rony Aparicio <u6009311@ad.utah.edu>
2017-11-08 11:11:29 -0700

    not yet ready

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require("express");
-const session = require("express-session");
 const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
-const passport = require("passport");
-const path = require('path');
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-const routes = require("./controllers/controller.js");
-const PORT = process.env.PORT || 8080;
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Serve up static assets
+app.use(express.static("client/build"));
+// Add routes, both API and view
+app.use(routes);
 
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+  {
+    useMongoClient: true
+  }
+);
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
