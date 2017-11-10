63a8fbbf8718128cc1b8847b7c3b9a28a9cd3776
Rony Aparicio <u6009311@ad.utah.edu>
2017-11-10 14:29:48 -0700

    front end index and app files

---
+++ client/src/app.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+  
+  
+  
+
+export default App;
