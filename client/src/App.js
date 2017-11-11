eb0ad0e345b7b07fc9f78a79fa36987f4b7a0c96
rony <ronyaparicio1@gmail.comgit config --global user.name ronygit commit -m 'l'git config --global user.email ronyaparicio1@gmail.com>
2017-11-10 21:13:11 -0700

    front end set up

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Mainpage from "./pages/Mainpage";
+import Profile from "./pages/Profile";
+
+const App = () =>
+    <Router>
+        <div>
+            <Switch>
+                <Route exact path="/" component={Welcome} />
+                <Route exact path="/movies" component={Mainpage} />
+                <Route exact path="/profile" component={Profile} />
+            </Switch>
+        </div>
+    </Router>;
+export default App;
--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.use(bodyParser.json());
 // Serve up static assets
 app.use(express.static("client/build"));
 // Add routes, both API and view
-app.use(routes);
+
 // Set up promises with mongoose
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+  process.env.MONGODB_URI || "mongodb://localhost/mymovielist",
   {
     useMongoClient: true
   }
