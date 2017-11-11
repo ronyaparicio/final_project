32bf4a18bc47b15d9459ec95d839c8d4faf66c2f
rony <ronyaparicio1@gmail.comgit config --global user.name ronygit commit -m 'l'git config --global user.email ronyaparicio1@gmail.com>
2017-11-11 11:15:32 -0700

    form properties for welcome page

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
 
 class Welcome extends Component {
+
+    state = {
+        name: [],
+        lastname: "",
+        email: ""
+    };
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+    handleFormSubmit = event => {
+        event.preventDefault();
+      
+    };
+
+
     render() {
         return (
-            <div> </div>
+            <div>
+                
+            </div>
         )
     }
 
