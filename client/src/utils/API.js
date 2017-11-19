8767cfaebb019f5846e81b8c312b00a3bff44cf3
Burke Simmons <burkesimmons@gmail.com>
2017-11-18 17:36:31 -0700

    Adding MovieDB functionality written by Cesar!

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,17 +1,38 @@
 import axios from  "axios";
 
+
+const SEARCHURL = `https://api.themoviedb.org/3/search/movie?api_key=dfc918e89334423f004fdc14fda75e92&query=zoolander`;
+
 export default {
     saveUser:(userData)=> {
-        return axios.post("/api/routes", userData);
+         axios.post("/api/authRoutes/register", userData)
+            .then((res)=> {
+                console.log(res);
+                let data = res.data;
+                if (data && data.token) {
+                    document.cookie = 'movieListUser='+data.token+'; Path=/;'
+                    localStorage.setItem('movieListUserId', data.id + '');
+                }
+            })
+    },
+    login: (userData)=> {
+       return axios.post("/api/authRoutes/login", userData);
+
+    },
+    exampleRequest: (movieData)=> {
+        let movieToken = document.cookie.split("movieListUser=")[1]
+        let headers = {headers: {"Authorization": movieToken}}
+        axios.post("/api/movie", movieData, headers)
+            
+    },
+    moviedetails: function(query) {
+      return axios.get(SEARCHURL);
+
+    },
+
+    topMovies: function(query) {
+        return axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=dfc918e89334423f004fdc14fda75e92&sort_by=popularity.desc&page=1`);
     }
 };
-
-// axios.get('/user?ID=12345')
-// .then(function (response) {
-//   console.log(response);
-// })
-// .catch(function (error) {
-//   console.log(error);
-// });
 
 
