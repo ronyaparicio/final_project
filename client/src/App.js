cac4a461c48e7a06a01fb178010d0cbf776061c2
rony <ronyaparicio1@gmail.comgit config --global user.name ronygit commit -m 'l'git config --global user.email ronyaparicio1@gmail.com>
2017-11-28 19:11:42 -0700

    app

--- client/src/App.js
+++ client/src/App.js
@@ -13,11 +13,6 @@ const App = () =>
                 <Route exact path="/" component={Welcome} />
                 <Route exact path="/movies" component={Mainpage} />
                 <Route exact path="/profile" component={Profile} />
-<<<<<<< HEAD
-=======
-                <Route exact path="/welcome" component={LandingPage} />
-                <Route exact path="/Action" component={ActionPage} />
->>>>>>> d0dbb81a05f8693747d87562c956f741ccb77c2a
             </Switch>
         </div>
     </Router>;
